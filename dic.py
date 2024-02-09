60427de2f0d1e2e007ba670e547adc9d48631382
prasad <durgaprasad04806@gmail.com>
2024-02-09 10:53:01 +0530

    added return statement

---
+++ dic.py
@@ -0,0 +1,18 @@
+"""
+my_set={1,2,3,4,5}
+dict={i:i**3 for i in my_set}
+print(dict)
+"""
+languages=["java","python","C"]
+ides=["Eclipse","pycharm","Codeblocks"]
+dict={java:Eclipse,python:pycharm,c:Codeblocks}
+zip=(languages**ides)
+print(zip)
+
+
+
+
+
+
+
+
